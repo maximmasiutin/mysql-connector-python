7220e5b8c4c8b2bbf7856285b63694cb06bf0e00
Oscar Pacheco <oscar.p.pacheco@oracle.com>
2024-06-13 14:09:39 -0600

    BUG#36731062: mysql_native_password plugin not bundled with the C-extension
    Starting with version 9.0.0 the mysql_native_password client-side
    plugin is required for connection to MySQL accounts that use
    native password authentication. That plugin is not bundled
    with the connector and hence it is impossible to make such a
    connection.
    
    With this patch, the mysql_native_password plugin is bundled
    like the connector bundles other authentication plugins such
    as authentication_kerberos_client and authentication_webauthn_client.
    
    Change-Id: Ic47de6a075b8789893330a18c5839e8c87605fcd

--- mysql-connector-python/cpydist/__init__.py
+++ mysql-connector-python/cpydist/__init__.py
@@ -283,6 +283,7 @@ class BaseCommand(Command):
                 ("Kerberos", f"authentication_kerberos_client.{plugin_ext}"),
                 ("OCI IAM", f"authentication_oci_client.{plugin_ext}"),
                 ("WebAuthn", f"authentication_webauthn_client.{plugin_ext}"),
+                ("MySQL Native", f"mysql_native_password.{plugin_ext}"),
             ]
 
             for plugin_name, plugin_file in plugin_list:
